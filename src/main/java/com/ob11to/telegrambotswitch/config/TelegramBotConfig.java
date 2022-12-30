8b114cbb28aa3f9d92423be77bb23216fe925539
OB11TO <OB11TO61@yandex.ru>
2022-12-30 13:02:49 +0300

    add init

---
+++ src/main/java/com/ob11to/telegrambotswitch/config/TelegramBotConfig.java
@@ -0,0 +1,20 @@
+package com.ob11to.telegrambotswitch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "telegram")
+public class TelegramBotConfig {
+    //configuration pulling properties from application.properties
+
+    //name received during registration
+    private String botUsername;
+    private String botToken;
+//    private String botPath;
+
+}
---
+++ src/test/java/com/ob11to/telegrambotswitch/TelegramBotSwitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ob11to.telegrambotswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotSwitchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
