4ea39c3c0c59d7ed9911ace7c182416b8b166fe9
OB11TO <OB11TO61@yandex.ru>
2023-01-27 15:13:18 +0300

    [Response] add response processor

---
+++ src/main/java/com/ob11to/telegrambotswitch/dto/Response.java
@@ -0,0 +1,32 @@
+package com.ob11to.telegrambotswitch.dto;
+
+import com.ob11to.telegrambotswitch.entity.ContentType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Response {
+
+    private ContentType contentType;
+
+    private String name;
+
+    private String path;
+
+    public InputStream getContentStream(){
+        try {
+            return new FileInputStream(path + "/" + name + "." + contentType);
+
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/com/ob11to/telegrambotswitch/service/ResponseProcessor.java
@@ -0,0 +1,39 @@
+package com.ob11to.telegrambotswitch.service;
+
+import com.ob11to.telegrambotswitch.dto.Request;
+import com.ob11to.telegrambotswitch.dto.Response;
+import com.ob11to.telegrambotswitch.entity.ContentType;
+import com.ob11to.telegrambotswitch.service.youtube.YouTubeDownloaderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ResponseProcessor {
+
+    private final YouTubeDownloaderService youtubeDownloader;
+    private final FolderManagerService folderManagerService;
+
+    public Response processResponse(Request request) {
+        boolean isAudio = ContentType.mp3.equals(request.getFormat());
+        String videoId = request.getVideoId();
+
+        ContentType type;
+
+        youtubeDownloader.download(request);
+
+        type = isAudio
+                ? ContentType.mp3
+                : ContentType.mp4;
+
+        return new Response(
+                type,
+                videoId,
+                folderManagerService.getPath()
+        );
+    }
+
+    public void stopDownload() {
+        youtubeDownloader.stopDownloading();
+    }
+}
